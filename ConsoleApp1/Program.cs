98c5b4504ea41fb97a6bdb1d6d89bcba36c779d6
Ankur Nigam <ankur.nigam198@live.com>
2017-08-04 23:24:06 +0530

    cleanup

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,23 +13,17 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             s = new StopWatch();
-            int numOfTimes = 500;
+            int numOfTimes = 1000;
             SParse parse = new SParse();
             parse.CreateTraditionalMatrix();
-            var a = parse.CreateSParseMy();
-            /*for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < a.Length/3; j++)
-                {
-                    Console.Write(a[i, j]+"\t");
-                }
-                Console.Write("\n");
-            }*/
+
+            Console.WriteLine("Press enter key to start benchmark");
+            Console.ReadLine();
 
             s.Start();
             for (int i = 0; i < numOfTimes; i++)
             {
-                parse.CreateSParseSir();
+                parse.CreateSParseMy();
             }
             s.Stop();
             Console.WriteLine("My Time is:" + s.Elasped().ToString());
